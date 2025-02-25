a95f2b4a3f596628e64d431b213d248e5212c8df
AngelCoyote-max <saraviaedson97@gmail.com>
2025-02-25 08:44:39 -0600

    se creo el repo

--- eddsn_saravia.py
+++ eddsn_saravia.py
@@ -1,1 +1,18 @@
-print ("hola mundo")
+#algoritmo de busqueda lineal
+def busqueda_lineal(lista, elemnto):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i #develve el indice del elemto encontrado
+        return -1 # devuelve -1 si el elemnto no se encuentra
+
+# Listade ejemplo
+numeros = [10, 20, 30, 40, 50]
+
+#Buscar un elemento
+elemento_buscado = int(input("ingresa el nuero a buscar: "))
+indice = busqueda_lineal(numeros,elemento_buscado)
+
+if indice != -1:
+    print(f"el numero {elemento_buscado} se encuentra en el indice {indice} ")
+else:
+    print(f"el numero {elemento_buscado} no s encuentr en la lista.")
