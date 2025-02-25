7d52ad45f64ef74f3a1d7eb742cdc7e8067daa7c
Ariel-12 <arielitoguti89@gmail.com>
2025-02-25 11:14:03 -0400

    Update ariel_lima.py

--- ariel_lima.py
+++ ariel_lima.py
@@ -1,1 +1,18 @@
-print ("hola mundo")
+#ARIELGIOVANNYLIMAGUTIERREZ
+
+def busqueda_lineal(lista, elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i
+    return -1
+
+numeros = [10, 20, 30, 40, 50]
+
+elemento_buscado = int(input("Ingresa el número a buscar: "))
+
+indice = busqueda_lineal(numeros, elemento_buscado)
+
+if indice != -1:
+    print(f"El número {elemento_buscado} se encuentra en el índice {indice}.")
+else:
+    print(f"El número {elemento_buscado} no se encuentra en la lista.")
