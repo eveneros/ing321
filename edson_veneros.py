d1203be88cdc118a4a651e8c68a1111837896a68
Edson Veneros <veneros.edson@gmail.com>
2025-02-26 10:03:54 -0400

    Creando una clase nueva

---
+++ edson_veneros.py
@@ -0,0 +1,16 @@
+class Algoritmos:
+    def __init__(self):
+        pass
+
+    def suma_numeros(self, a, b):
+        """Suma de dos numeros, variables a y b"""
+        c= a + b
+        print(f"resultado: {c}")
+
+
+algoritmos = Algoritmos()
+algoritmos.suma_numeros(1,2)
+
+
+
+    
