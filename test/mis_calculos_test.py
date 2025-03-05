e0224f8e002ff224c49a240feb362dbd549ae053
Edson Veneros <veneros.edson@gmail.com>
2025-03-04 20:58:45 -0400

    utilizando uniitest

---
+++ test/mis_calculos_test.py
@@ -0,0 +1,11 @@
+import unittest
+from mis_calculos import Calculos
+
+class ProbarCalculos(unittest.TestCase):
+
+    def test_suma(self):
+        calculos = Calculos(8, 2)
+        self.assertEqual(calculos.get_suma(), 10, 'La suma tiene error.')
+
+if __name__ == '__main__':
+    unittest.main()
