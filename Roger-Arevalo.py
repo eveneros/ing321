046967251a945874916a5748a98c33bc3d057ad5
Ro-Are-Lo <roger.arevalo.51334@usalesiana.edu.bo>
2025-03-11 23:27:40 -0400

    laboratorio 4

--- Roger-Arevalo.py
+++ Roger-Arevalo.py
@@ -1,16 +1,16 @@
-def b-lin(lista.elem):
-    for i in range(len(lsita)):
+def busqueda_lineal(lista,elem):
+    for i in range(len(lista)):
         if lista[i] == elem:
-            return 1
+            return i
     return -1
 
 num = [1,2,3,4,5]
-elem = int(input("ingresar el numero a buscar "))
-indice = b-lin( num, elem)
+elem_b = int(input("ingresar el numero a buscar "))
+indice = busqueda_lineal( num, elem_b)
 
 if indice != -1:
-    print("el numero ",elem," se encuentra en el indice ",indice)
-else
-    print("el numero ",elem," no se encuentra en la lista")
+    print("el numero ",elem_b," se encuentra en el indice ",indice)
+else:
+    print("el numero ",elem_b," no se encuentra en la lista")
 
 
