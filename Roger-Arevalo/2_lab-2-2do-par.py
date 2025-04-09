959779b173ba2482fafbd6157d8b00845e688664
Ro-Are-Lo <roger.arevalo.51334@usalesiana.edu.bo>
2025-04-09 10:50:58 -0400

    lab 2 par 2

---
+++ Roger-Arevalo/2_lab-2-2do-par.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    posts = response.json()
+    return sorted(posts, key=lambda x: x["id"])
+
+def binary_search_posts(posts, target_id):
+    left, right = 0, len(posts) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if posts[mid]["id"] == target_id:
+            return posts[mid]
+        elif posts[mid]["id"] < target_id:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return None
+
+if __name__ == "__main__":
+    posts = get_posts()
+    target_post = binary_search_posts(posts, 42)
+    print(target_post)
