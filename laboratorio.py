7763ec58209a9eebb75099603662e953f600fe04
AngelCoyote-max <saraviaedson97@gmail.com>
2025-04-09 22:50:40 -0400

    Add files via upload

---
+++ laboratorio.py
@@ -0,0 +1,18 @@
+import requests
+
+def get_starwars_characters ():
+    url = "https://swapi.dev/api/people"
+    response = requests.get(url)
+    return response.json()["results"] # Lista de pers on ajes
+# Crear tabla de hash (diccion ario) para búsqueda rápida
+def create_hash_table(characters) :
+    return {character ["name" ]: character for character in characters}
+# Uso:
+characters = get_starwars_characters()
+hash_table = create_hash_table(characters)
+
+def search_character (name) :
+    return hash_table.get (name, "Personaje no encontrado")
+    
+# Ejemplo:
+print (search_character ("Luke Skywalker")) # Devuelve todos los datos de Luke
