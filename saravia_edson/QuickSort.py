0b6a7a9ddc16102658077e877ca83deb80a71d12
Ángel Coyote-max <saraviaedson97@gmail.com>
2025-04-16 13:10:24 -0400

    codigo corregido

---
+++ saravia_edson/QuickSort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort (left) + middle + quick_sort(right)
+
+# Ejemplo:
+arr = [3, 6, 8, 18, 1, 2, 1]
+print("Quick Sort:", quick_sort(arr))
