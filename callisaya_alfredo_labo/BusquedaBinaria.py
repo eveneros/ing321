5f3c3235f8413f66d72ffb5ad812f664ef22cfa7
AlfredoCallisaya <juan.callisaya.57751@usalesiana.edu.bo>
2025-04-16 20:13:37 -0400

    Estaba en choque por eso no pude hacer en clases

---
+++ callisaya_alfredo_labo/BusquedaBinaria.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target, low, high):
+    if low > high:
+        return -1
+    mid = (low + high)//2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search(arr, target, mid+1, high)
+    else:
+        return binary_search(arr, target, low, mid-1)
+
+#Ejemplo
+arr = [2,5,8,12,16,23,38,56]
+target = 16
+result = binary_search(arr,target,0, len(arr)-1)
+print ("Elemento encontrado en indice: ", result)
